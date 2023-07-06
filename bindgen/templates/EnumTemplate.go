d4efb571db85a22ba7c8a766d90d5375a80445ca
Jakub Janowski <jakub.janowski@nordsec.com>
2023-07-06 10:40:42 +0300

    Remove unnecessery import from EnumTemplate.go. It doubles the one in wrapper.go and in case go fmt is not used it breakes bindings compilation.

--- bindgen/templates/EnumTemplate.go
+++ bindgen/templates/EnumTemplate.go
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */#}
 
-{{ self.add_import("encoding/binary") -}}
 {% let e = ci.get_enum_definition(name).unwrap() -%}
 {%- if e.is_flat() -%}
 
