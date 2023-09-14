18d6098fbe3adc4255fe196adab2ee3997f33cab
dignifiedquire <me@dignifiedquire.com>
2023-09-14 20:58:29 +0200

    stub out foreign exectuor types

--- bindgen/templates/BridgingHeaderTemplate.h
+++ bindgen/templates/BridgingHeaderTemplate.h
@@ -13,15 +13,15 @@
 // We ensure they are declared exactly once, with a header guard, UNIFFI_SHARED_H.
 #ifdef UNIFFI_SHARED_H
 	// We also try to prevent mixing versions of shared uniffi header structs.
-	// If you add anything to the #else block, you must increment the version suffix in UNIFFI_SHARED_HEADER_V4
-	#ifndef UNIFFI_SHARED_HEADER_V4
+	// If you add anything to the #else block, you must increment the version suffix in UNIFFI_SHARED_HEADER_V5
+	#ifndef UNIFFI_SHARED_HEADER_V5
 		#error Combining helper code from multiple versions of uniffi is not supported
-	#endif // ndef UNIFFI_SHARED_HEADER_V4
+	#endif // ndef UNIFFI_SHARED_HEADER_V5
 #else
 #define UNIFFI_SHARED_H
-#define UNIFFI_SHARED_HEADER_V4
+#define UNIFFI_SHARED_HEADER_V5
 // ⚠️ Attention: If you change this #else block (ending in `#endif // def UNIFFI_SHARED_H`) you *must* ⚠️
-// ⚠️ increment the version suffix in all instances of UNIFFI_SHARED_HEADER_V4 in this file.           ⚠️
+// ⚠️ increment the version suffix in all instances of UNIFFI_SHARED_HEADER_V5 in this file.           ⚠️
 
 typedef struct RustBuffer {
 	int32_t capacity;
@@ -30,6 +30,19 @@ typedef struct RustBuffer {
 } RustBuffer;
 
 typedef int32_t (*ForeignCallback)(uint64_t, int32_t, RustBuffer, RustBuffer *);
+
+// Task defined in Rust that Swift executes
+typedef void (*RustTaskCallback)(const void *, int8_t);
+
+// Callback to execute Rust tasks using a Go routine
+//
+// Args:
+//   executor: ForeignExecutor lowered into a uint64_t value
+//   delay: Delay in MS
+//   task: RustTaskCallback to call
+//   task_data: data to pass the task callback
+typedef int8_t (*ForeignExecutorCallback)(uint64_t, uint32_t, RustTaskCallback, const void *);
+
 
 typedef struct ForeignBytes {
 	int32_t len;
@@ -43,7 +56,7 @@ typedef struct RustCallStatus {
 } RustCallStatus;
 
 // ⚠️ Attention: If you change this #else block (ending in `#endif // def UNIFFI_SHARED_H`) you *must* ⚠️
-// ⚠️ increment the version suffix in all instances of UNIFFI_SHARED_HEADER_V4 in this file.           ⚠️
+// ⚠️ increment the version suffix in all instances of UNIFFI_SHARED_HEADER_V5 in this file.           ⚠️
 #endif // def UNIFFI_SHARED_H
 
 {% for func in ci.iter_ffi_function_definitions() -%}
--- bindgen/templates/ForeignExecutorTemplate.go
+++ bindgen/templates/ForeignExecutorTemplate.go
@@ -5,8 +5,35 @@ const uniffiForeignExecutorCallbackCanceled byte = 1
 const uniffiForeignExecutorCallbackError byte = 2
 
 // Encapsulates an executor that can run Rust tasks
-type UniFfiForeignExecutor struct {
-	priority int
+type UniFfiForeignExecutor struct {}
+
+var FfiConverterForeignExecutorINSTANCE = UniFfiForeignExecutor{}
+
+func uniffiInitForeignExecutor() {
+	// TODO?
 }
 
+func (c UniFfiForeignExecutor) lower(value UniFfiForeignExecutor) C.int {
+	// TODO: pointer cast & pin memory
+	return 0
+}
+
+func (c UniFfiForeignExecutor) write(writer io.Writer, value UniFfiForeignExecutor) {
+	// TODO: unclear how to do this
+}
+
+func (c UniFfiForeignExecutor) lift(value C.int) UniFfiForeignExecutor {
+	// TODO: cast pointer back
+	return UniFfiForeignExecutor {}
+}
+
+func (c UniFfiForeignExecutor) read(reader io.Reader) UniFfiForeignExecutor {
+	// TODO: unclear how to to this
+	return UniFfiForeignExecutor {}
+}
+
+
+type {{ type_|ffi_destroyer_name }} struct {}
+
+func ({{ type_|ffi_destroyer_name }}) destroy(_ {{ type_name }}) {}
 
