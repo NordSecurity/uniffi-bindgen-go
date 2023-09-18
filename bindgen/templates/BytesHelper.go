e97161ee27a96c552fa6ebd3dc42b1388c127751
dignifiedquire <me@dignifiedquire.com>
2023-09-18 11:55:42 +0200

    fix bytes helper impl

--- bindgen/templates/BytesHelper.go
+++ bindgen/templates/BytesHelper.go
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */#}
 
+{{- self.add_import("math") }}
+
 type FfiConverterBytes struct{}
 
 var FfiConverterBytesINSTANCE = FfiConverterBytes{}
@@ -11,21 +13,40 @@ func (c FfiConverterBytes) lower(value []byte) C.RustBuffer {
 }
 
 func (c FfiConverterBytes) write(writer io.Writer, value []byte) {
-	if _, err := writer.Write(value); err != nil {
+	if len(value) > math.MaxInt32 {
+		panic("[]byte is too large to fit into Int32")
+	}
+
+	writeInt32(writer, int32(len(value)))
+	write_length, err := writer.Write(value)
+	if err != nil {
 		panic(err)
+	}
+	if write_length != len(value) {
+		panic(fmt.Errorf("bad write length when writing string, expected %d, written %d", len(value), write_length))
 	}
 }
 
 func (c FfiConverterBytes) lift(value C.RustBuffer) []byte {
-	return cRustBufferToGoBytes(value)
+	reader := fromCRustBuffer(value).asReader()
+	b, err := io.ReadAll(reader)
+	if err != nil {
+		panic(fmt.Errorf("reading reader: %w", err))
+	}
+	return b
 }
 
 func (c FfiConverterBytes) read(reader io.Reader) []byte {
-	result, err := io.ReadAll(reader)
+	length := readInt32(reader)
+	buffer := make([]byte, length)
+	read_length, err := reader.Read(buffer)
 	if err != nil {
 		panic(err)
 	}
-	return result
+	if read_length != int(length) {
+		panic(fmt.Errorf("bad read length when reading []byte, expected %d, read %d", length, read_length))
+	}
+	return buffer
 }
 
 type {{ type_|ffi_destroyer_name }} struct {}
