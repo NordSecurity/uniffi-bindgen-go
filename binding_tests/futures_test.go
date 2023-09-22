1396b61f934f250ad3f5b4f2bc779ab74f0338ab
dignifiedquire <me@dignifiedquire.com>
2023-09-22 16:20:26 +0200

    implement basic future test

--- binding_tests/futures_test.go
+++ binding_tests/futures_test.go
@@ -4,273 +4,190 @@
 
 package binding_tests
 
-// import (
-// 	"testing"
-
-// 	. "github.com/NordSecurity/uniffi-bindgen-go/binding_tests/generated/futures/futures"
-// 	"github.com/stretchr/testify/assert"
-// )
-
-// func TestFutures(t *testing.T) {
-// 	counter := DispatchGroup()
-
-// 	// Test `alwaysReady`
-// 	counter.Enter()
-
-// 	// Task {
-// 	// 	let t0 = Date()
-// 	// 	let result = await alwaysReady()
-// 	// 	let t1 = Date()
-
-// 	// 	let tDelta = DateInterval(start: t0, end: t1)
-// 	// 	assert(tDelta.duration < 0.1)
-// 	// 	assert(result == true)
-
-// 	// 	counter.leave()
-// 	// }
-
-// 	// // Test record.
-// 	// counter.enter()
-
-// 	// Task {
-// 	// 	let result = await newMyRecord(a: "foo", b: 42)
-
-// 	// 	assert(result.a == "foo")
-// 	// 	assert(result.b == 42)
-
-// 	// 	counter.leave()
-// 	// }
-
-// 	// // Test `void`
-// 	// counter.enter()
-
-// 	// Task {
-// 	// 	let t0 = Date()
-// 	// 	await void()
-// 	// 	let t1 = Date()
-
-// 	// 	let tDelta = DateInterval(start: t0, end: t1)
-// 	// 	assert(tDelta.duration < 0.1)
-
-// 	// 	counter.leave()
-// 	// }
-
-// 	// // Test `Sleep`
-// 	// counter.enter()
-
-// 	// Task {
-// 	// 	let t0 = Date()
-// 	// 	let result = await sleep(ms: 2000)
-// 	// 	let t1 = Date()
-
-// 	// 	let tDelta = DateInterval(start: t0, end: t1)
-// 	// 	assert(tDelta.duration > 2 && tDelta.duration < 2.1)
-// 	// 	assert(result == true)
-
-// 	// 	counter.leave()
-// 	// }
-
-// 	// // Test sequential futures.
-// 	// counter.enter()
-
-// 	// Task {
-// 	// 	let t0 = Date()
-// 	// 	let result_alice = await sayAfter(ms: 1000, who: "Alice")
-// 	// 	let result_bob = await sayAfter(ms: 2000, who: "Bob")
-// 	// 	let t1 = Date()
-
-// 	// 	let tDelta = DateInterval(start: t0, end: t1)
-// 	// 	assert(tDelta.duration > 3 && tDelta.duration < 3.1)
-// 	// 	assert(result_alice == "Hello, Alice!")
-// 	// 	assert(result_bob == "Hello, Bob!")
-
-// 	// 	counter.leave()
-// 	// }
-
-// 	// // Test concurrent futures.
-// 	// counter.enter()
-
-// 	// Task {
-// 	// 	async let alice = sayAfter(ms: 1000, who: "Alice")
-// 	// 	async let bob = sayAfter(ms: 2000, who: "Bob")
-
-// 	// 	let t0 = Date()
-// 	// 	let (result_alice, result_bob) = await (alice, bob)
-// 	// 	let t1 = Date()
-
-// 	// 	let tDelta = DateInterval(start: t0, end: t1)
-// 	// 	assert(tDelta.duration > 2 && tDelta.duration < 2.1)
-// 	// 	assert(result_alice == "Hello, Alice!")
-// 	// 	assert(result_bob == "Hello, Bob!")
-
-// 	// 	counter.leave()
-// 	// }
-
-// 	// // Test async methods
-// 	// counter.enter()
-
-// 	// Task {
-// 	// 	let megaphone = newMegaphone()
-
-// 	// 	let t0 = Date()
-// 	// 	let result_alice = await megaphone.sayAfter(ms: 2000, who: "Alice")
-// 	// 	let t1 = Date()
-
-// 	// 	let tDelta = DateInterval(start: t0, end: t1)
-// 	// 	assert(tDelta.duration > 2 && tDelta.duration < 2.1)
-// 	// 	assert(result_alice == "HELLO, ALICE!")
-
-// 	// 	counter.leave()
-// 	// }
-
-// 	// // Test async function returning an object
-// 	// counter.enter()
-
-// 	// Task {
-// 	// 	let megaphone = await asyncNewMegaphone()
-
-// 	// 	let result = try await megaphone.fallibleMe(doFail: false)
-// 	// 	assert(result == 42)
-
-// 	// 	counter.leave()
-// 	// }
-
-// 	// // Test with the Tokio runtime.
-// 	// counter.enter()
-
-// 	// Task {
-// 	// 	let t0 = Date()
-// 	// 	let result_alice = await sayAfterWithTokio(ms: 2000, who: "Alice")
-// 	// 	let t1 = Date()
-
-// 	// 	let tDelta = DateInterval(start: t0, end: t1)
-// 	// 	assert(tDelta.duration > 2 && tDelta.duration < 2.1)
-// 	// 	assert(result_alice == "Hello, Alice (with Tokio)!")
-
-// 	// 	counter.leave()
-// 	// }
-
-// 	// // Test fallible function/method…
-// 	// // … which doesn't throw.
-// 	// counter.enter()
-
-// 	// Task {
-// 	// 	let t0 = Date()
-// 	// 	let result = try await fallibleMe(doFail: false)
-// 	// 	let t1 = Date()
-
-// 	// 	let tDelta = DateInterval(start: t0, end: t1)
-// 	// 	assert(tDelta.duration > 0 && tDelta.duration < 0.1)
-// 	// 	assert(result == 42)
-
-// 	// 	counter.leave()
-// 	// }
-
-// 	// Task {
-// 	// 	let m = try await fallibleStruct(doFail: false)
-// 	// 	let result = try await m.fallibleMe(doFail: false)
-// 	// 	assert(result == 42)
-// 	// }
-
-// 	// counter.enter()
-
-// 	// Task {
-// 	// 	let megaphone = newMegaphone()
-
-// 	// 	let t0 = Date()
-// 	// 	let result = try await megaphone.fallibleMe(doFail: false)
-// 	// 	let t1 = Date()
-
-// 	// 	let tDelta = DateInterval(start: t0, end: t1)
-// 	// 	assert(tDelta.duration > 0 && tDelta.duration < 0.1)
-// 	// 	assert(result == 42)
-
-// 	// 	counter.leave()
-// 	// }
-
-// 	// // … which does throw.
-// 	// counter.enter()
-
-// 	// Task {
-// 	// 	let t0 = Date()
-
-// 	// 	do {
-// 	// 		let _ = try await fallibleMe(doFail: true)
-// 	// 	} catch MyError.Foo {
-// 	// 		assert(true)
-// 	// 	} catch {
-// 	// 		assert(false) // should never be reached
-// 	// 	}
-
-// 	// 	let t1 = Date()
-
-// 	// 	let tDelta = DateInterval(start: t0, end: t1)
-// 	// 	assert(tDelta.duration > 0 && tDelta.duration < 0.1)
-
-// 	// 	counter.leave()
-// 	// }
-
-// 	// Task {
-// 	// 	do {
-// 	// 		let _ = try await fallibleStruct(doFail: true)
-// 	// 	} catch MyError.Foo {
-// 	// 		assert(true)
-// 	// 	} catch {
-// 	// 		assert(false)
-// 	// 	}
-// 	// }
-
-// 	// counter.enter()
-
-// 	// Task {
-// 	// 	let megaphone = newMegaphone()
-
-// 	// 	let t0 = Date()
-
-// 	// 	do {
-// 	// 		let _ = try await megaphone.fallibleMe(doFail: true)
-// 	// 	} catch MyError.Foo {
-// 	// 		assert(true)
-// 	// 	} catch {
-// 	// 		assert(false) // should never be reached
-// 	// 	}
-
-// 	// 	let t1 = Date()
-
-// 	// 	let tDelta = DateInterval(start: t0, end: t1)
-// 	// 	assert(tDelta.duration > 0 && tDelta.duration < 0.1)
-
-// 	// 	counter.leave()
-// 	// }
-
-// 	// // Test a future that uses a lock and that is cancelled.
-// 	// counter.enter()
-// 	// Task {
-// 	// 	let task = Task {
-// 	// 	    try! await useSharedResource(options: SharedResourceOptions(releaseAfterMs: 100, timeoutMs: 1000))
-// 	// 	}
-
-// 	// 	// Wait some time to ensure the task has locked the shared resource
-// 	// 	try await Task.sleep(nanoseconds: 50_000_000)
-// 	// 	// Cancel the job task the shared resource has been released.
-// 	// 	//
-// 	// 	// FIXME: this test currently passes because `test.cancel()` doesn't actually cancel the
-// 	// 	// operation.  We need to rework the Swift async handling to handle this properly.
-// 	// 	task.cancel()
-
-// 	// 	// Try accessing the shared resource again.  The initial task should release the shared resource
-// 	// 	// before the timeout expires.
-// 	// 	try! await useSharedResource(options: SharedResourceOptions(releaseAfterMs: 0, timeoutMs: 1000))
-// 	// 	counter.leave()
-// 	// }
-
-// 	// // Test a future that uses a lock and that is not cancelled.
-// 	// counter.enter()
-// 	// Task {
-// 	// 	try! await useSharedResource(options: SharedResourceOptions(releaseAfterMs: 100, timeoutMs: 1000))
-// 	// 	try! await useSharedResource(options: SharedResourceOptions(releaseAfterMs: 0, timeoutMs: 1000))
-// 	// 	counter.leave()
-// 	// }
-
-// 	// counter.wait()
-// }
+import (
+	"testing"
+	"time"
+	"sync"
+
+	. "github.com/NordSecurity/uniffi-bindgen-go/binding_tests/generated/futures/futures"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFutures(t *testing.T) {
+	// Test `alwaysReady`
+	t0 := time.Now()
+	result := AlwaysReady()
+	t1 := time.Now()
+
+	assert.True(t, t1.Sub(t0) < 1 * time.Millisecond)
+	assert.True(t, result)
+
+	// Test record.
+	result = NewMyRecord("foo", 42)
+	assert.Equal(t, result.A, "foo")
+	assert.Equal(t, result.B, 42)
+
+	// Test `void`
+	t0 = time.Now()
+	Void()
+	t1 = time.Now()
+	assert.True(t, t1.Sub(t0) < 1 * time.Millisecond)
+
+	// Test `Sleep`
+	t0 = time.Now()
+	result = Sleep(2000)
+	t1 = time.Now()
+
+	elapsed := t1.Sub(t0)
+	assert.True(t, elapsed < 2010 * time.Millisecond)
+	assert.True(t, elapsed > 2000 * time.Millisecond)
+	assert.True(t, result)
+
+	// Test sequential futures.
+	t0 = time.Now()
+	resultAlice := SayAfter(1000, "Alice")
+	resultBob := SayAfter(2000, "Bob")
+	t1 = time.Now()
+
+	elapsed = t1.Sub(t0)
+	assert.True(t, elapsed < 3010 * time.Millisecond)
+	assert.True(t, elapsed > 3000 * time.Millisecond)
+	assert.Equal(t, resultAlice, "Hello, Alice!")
+	assert.Equal(t, resultBob, "Hello, Bob!")
+
+	// Test concurrent futures.
+	var wg sync.WaitGroup
+
+	t0 = time.Now()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		result := SayAfter(1000, "Alice")
+		assert.Equal(t, result, "Hello, Alice!")
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		result := SayAfter(2000, "Bob")
+		assert.Equal(t, result, "Hello, Bob!")
+	}()
+
+	wg.Wait()
+	t1 = time.Now()
+	elapsed = t1.Sub(t0)
+	assert.True(t, elapsed < 2010 * time.Millisecond)
+	assert.True(t, elapsed > 2000 * time.Millisecond)
+
+	// Test async methods
+	// 	let megaphone = newMegaphone()
+
+	// 	let t0 = Date()
+	// 	let result_alice = await megaphone.sayAfter(ms: 2000, who: "Alice")
+	// 	let t1 = Date()
+
+	// 	let tDelta = DateInterval(start: t0, end: t1)
+	// 	assert(tDelta.duration > 2 && tDelta.duration < 2.1)
+	// 	assert(result_alice == "HELLO, ALICE!")
+
+	// Test async function returning an object
+	// 	let megaphone = await asyncNewMegaphone()
+
+	// 	let result = try await megaphone.fallibleMe(doFail: false)
+	// 	assert(result == 42)
+
+	// Test with the Tokio runtime.
+	// 	let t0 = Date()
+	// 	let result_alice = await sayAfterWithTokio(ms: 2000, who: "Alice")
+	// 	let t1 = Date()
+
+	// 	let tDelta = DateInterval(start: t0, end: t1)
+	// 	assert(tDelta.duration > 2 && tDelta.duration < 2.1)
+	// 	assert(result_alice == "Hello, Alice (with Tokio)!")
+
+	// Test fallible function/method…
+	// … which doesn't throw.
+	// 	let t0 = Date()
+	// 	let result = try await fallibleMe(doFail: false)
+	// 	let t1 = Date()
+
+	// 	let tDelta = DateInterval(start: t0, end: t1)
+	// 	assert(tDelta.duration > 0 && tDelta.duration < 0.1)
+	// 	assert(result == 42)
+
+	// 	let m = try await fallibleStruct(doFail: false)
+	// 	let result = try await m.fallibleMe(doFail: false)
+	// 	assert(result == 42)
+
+	// 	let megaphone = newMegaphone()
+
+	// 	let t0 = Date()
+	// 	let result = try await megaphone.fallibleMe(doFail: false)
+	// 	let t1 = Date()
+
+	// 	let tDelta = DateInterval(start: t0, end: t1)
+	// 	assert(tDelta.duration > 0 && tDelta.duration < 0.1)
+	// 	assert(result == 42)
+
+	// … which does throw.
+	// 	let t0 = Date()
+
+	// 	do {
+	// 		let _ = try await fallibleMe(doFail: true)
+	// 	} catch MyError.Foo {
+	// 		assert(true)
+	// 	} catch {
+	// 		assert(false) // should never be reached
+	// 	}
+
+	// 	let t1 = Date()
+
+	// 	let tDelta = DateInterval(start: t0, end: t1)
+	// 	assert(tDelta.duration > 0 && tDelta.duration < 0.1)
+
+	// 	do {
+	// 		let _ = try await fallibleStruct(doFail: true)
+	// 	} catch MyError.Foo {
+	// 		assert(true)
+	// 	} catch {
+	// 		assert(false)
+	// 	}
+
+	// 	let megaphone = newMegaphone()
+
+	// 	let t0 = Date()
+
+	// 	do {
+	// 		let _ = try await megaphone.fallibleMe(doFail: true)
+	// 	} catch MyError.Foo {
+	// 		assert(true)
+	// 	} catch {
+	// 		assert(false) // should never be reached
+	// 	}
+
+	// 	let t1 = Date()
+
+	// 	let tDelta = DateInterval(start: t0, end: t1)
+	// 	assert(tDelta.duration > 0 && tDelta.duration < 0.1)
+
+	// Test a future that uses a lock and that is cancelled.
+	// 	let task = Task {
+	// 	    try! await useSharedResource(options: SharedResourceOptions(releaseAfterMs: 100, timeoutMs: 1000))
+	// 	}
+
+	// 	// Wait some time to ensure the task has locked the shared resource
+	// 	try await Task.sleep(nanoseconds: 50_000_000)
+	// 	// Cancel the job task the shared resource has been released.
+	// 	//
+	// 	// FIXME: this test currently passes because `test.cancel()` doesn't actually cancel the
+	// 	// operation.  We need to rework the Swift async handling to handle this properly.
+	// 	task.cancel()
+
+	// 	// Try accessing the shared resource again.  The initial task should release the shared resource
+	// 	// before the timeout expires.
+	// 	try! await useSharedResource(options: SharedResourceOptions(releaseAfterMs: 0, timeoutMs: 1000))
+
+	// Test a future that uses a lock and that is not cancelled.
+	// 	try! await useSharedResource(options: SharedResourceOptions(releaseAfterMs: 100, timeoutMs: 1000))
+	// 	try! await useSharedResource(options: SharedResourceOptions(releaseAfterMs: 0, timeoutMs: 1000))
+}
