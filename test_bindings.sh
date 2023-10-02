2fa1f812eebd74096d4df84d08a162e9de1dbe34
dignifiedquire <me@dignifiedquire.com>
2023-10-02 15:36:05 +0200

    ci..

--- test_bindings.sh
+++ test_bindings.sh
@@ -4,10 +4,10 @@ set -euxo pipefail
 SCRIPT_DIR="${SCRIPT_DIR:-$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )}"
 ROOT_DIR="$SCRIPT_DIR"
 
-BINDINGS_DIR="$ROOT_DIR/binding_tests/generated"
+BINDINGS_DIR="$ROOT_DIR/binding_tests"
 BINARIES_DIR="$ROOT_DIR/target/debug"
 
-pushd $BINDINGS_DIR/..
+pushd $BINDINGS_DIR
 LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$BINARIES_DIR" \
 	CGO_LDFLAGS="-luniffi_fixtures -L$BINARIES_DIR -lm -ldl" \
 	CGO_ENABLED=1 \
