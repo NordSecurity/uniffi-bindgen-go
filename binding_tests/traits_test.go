4ce969efc1cdd81950203c639f7d913bb0a3893d
povilas.nagrockas <povilas.nagrockas@nordsec.com>
2025-01-29 13:38:37 +0200

    Add traits test
    Signed-off-by: povilas.nagrockas <povilas.nagrockas@nordsec.com>

---
+++ binding_tests/traits_test.go
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package binding_tests
+
+import (
+	"testing"
+
+	. "github.com/NordSecurity/uniffi-bindgen-go/binding_tests/generated/traits"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type goButton struct{}
+
+func (btn goButton) Name() string {
+	return "GoButton"
+}
+
+func TestTraits(t *testing.T) {
+	for _, button := range GetButtons() {
+		name := button.Name()
+		// Check that the name is one of the expected values
+		assert.Contains(t, []string{"go", "stop"}, name)
+		// Check that we can round-trip the button through Rust
+		assert.Equal(t, Press(button).Name(), name)
+	}
+
+	assert.Equal(t, "GoButton", Press(goButton{}).Name())
+}
