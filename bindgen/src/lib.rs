6ea9071641dcf5ff99d85494725853aa2e5e16b9
povilas.nagrockas <povilas.nagrockas@nordsec.com>
2025-03-27 20:36:20 +0200

    Added support for all uniffi traits
    Signed-off-by: povilas.nagrockas <povilas.nagrockas@nordsec.com>

--- bindgen/src/lib.rs
+++ bindgen/src/lib.rs
@@ -98,14 +98,11 @@ impl uniffi_bindgen::BindingGenerator for BindingGeneratorGo {
             let bindings_path = full_bindings_path(config, &settings.out_dir);
             fs::create_dir_all(&bindings_path)?;
             let go_file = bindings_path.join(format!("{}.go", ci.namespace()));
-            let (header, c_file_content, wrapper) = generate_go_bindings(&config, &ci)?;
+            let (header, wrapper) = generate_go_bindings(&config, &ci)?;
             fs::write(&go_file, wrapper)?;
 
             let header_file = bindings_path.join(config.header_filename());
             fs::write(header_file, header)?;
-
-            let c_file = bindings_path.join(config.c_filename());
-            fs::write(c_file, c_file_content)?;
 
             if settings.try_format_code {
                 match Command::new("go").arg("fmt").arg(&go_file).output() {
--- fixtures/objects/src/lib.rs
+++ fixtures/objects/src/lib.rs
@@ -4,10 +4,7 @@
 
 use crossbeam::channel::{Receiver, Sender};
 use once_cell::sync::Lazy;
-use std::{
-    sync::{Arc, Mutex},
-    time::Duration,
-};
+use std::sync::{Arc, Mutex};
 
 static RECEIVER_COUNT: Lazy<Mutex<i32>> = Lazy::new(|| Mutex::new(0));
 
